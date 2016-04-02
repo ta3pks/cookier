6d1ef37e35424b538b32a4aea81f0b4f21e6eee7
nikos <nikos.efthias@gmail.com>
2016-04-03 02:40:40 +0300

    path option was added

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ _or alternatively_<br/>
 ```javascript
 cookier
 	->parse:function(cookieName) //cookiename is optional if not given returns an object with all cookies
-	->make:function(name,value,expiresInX_Days)
+	->make:function(name,value[,expiresInX_Days=1] [, path=currentPath])
+	//by degault expires is 1 day and the path is current path
 	->rm:function(cookieName)//removes a cookie
 ```
--- cookier.js
+++ cookier.js
@@ -1,20 +1,26 @@
-var cookier={
-	parse : function (cookieName){
-		var a=document.cookie.replace(/; /g,";").split(";"),
-		b=a.length,c={},nm=cookieName||!1;
-		while(b--){
-			var d=a[b].split(/=(.+)/);
-			c[d[0]]=d[1];
+var cookier = {
+	parse: function (cookieName) {
+		var a = document.cookie.replace(/; /g, ";").split(";"),
+			b = a.length,
+			c = {},
+			nm = cookieName || !1;
+		while (b--) {
+			var d = a[b].split(/=(.+)/);
+			c[d[0]] = d[1];
 		}
-		return (nm)?c[nm]:c;
+		return (nm) ? c[nm] : c;
 	},
-	make:function(name,value,expiresInX_days){
-		var a=new Date;a.setTime(Date.now()+(1000*60*60*24*expiresInX_days));
-		return (document.cookie=name+"="+value+";"+a.toUTCString())?!0:!1;
+	make: function (name, value, expiresInX_days, path) {
+		var a = new Date;
+		var expires = expiresInX_days || 1;
+		a.setTime(Date.now() + (1000 * 60 * 60 * 24 * expires));
+		var pt = path ? " ; path=" + path + ";" : ";";
+		return (document.cookie = name + "=" + value + ";" + "expires=" + a.toUTCString() + pt) ? !0 : !1;
 	},
-	rm:function(cookieName){
-		var a=new Date;a.setTime(0);
-		return (document.cookie=cookieName+"=;"+a.toUTCString())?!0:!1;
+	rm: function (cookieName) {
+		var a = new Date;
+		a.setTime(0);
+		return (document.cookie = cookieName + "=;" + a.toUTCString()) ? !0 : !1;
 	}
 
 }
